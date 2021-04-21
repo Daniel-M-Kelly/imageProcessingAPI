caca4979b76e3237ba77d794ddde30e268e05b0e
Daniel Kelly <email@danielmkelly.com>
2021-04-20 17:50:11 -0700

    feat: Process image

--- src/routes/api/imageProc.ts
+++ src/routes/api/imageProc.ts
@@ -1,9 +1,37 @@
 import express from 'express';
+import sharp from 'sharp';
+import fs from 'fs';
 
 const imageProc = express.Router();
 
-imageProc.get('/', (req, res) => {
-	res.send('Process Image');
-});
+imageProc.get(
+	'/',
+	async (req: express.Request, res: express.Response): Promise<void> => {
+		const name = req.query.name;
+		const width = parseInt((req.query.width as unknown) as string);
+		const height = parseInt((req.query.height as unknown) as string);
+
+		console.log(`Resizing Image ${name} to ${width}px x ${height}px`);
+
+		if (fs.existsSync(`./assets/thumb/${name}`)) {
+			console.log('File already cached!');
+			//send image
+			res.sendFile(`./assets/thumb/${name}`, { root: __dirname });
+		} else {
+			try {
+				const filePath = `./assets/full/${name}`;
+				await sharp(filePath)
+					.resize(width, height)
+					.toFile(`./assets/thumb/${name}`);
+				console.log('File Successfully resized and cached');
+				//send image
+				res.sendFile(`./assets/thumb/${name}`);
+			} catch (err) {
+				console.log(err);
+				res.send('Failure');
+			}
+		}
+	}
+);
 
 export default imageProc;
